2bbf7b3c727863dacbc91d6cd6886d5af4fd852e
Bart Nagel <bart@tremby.net>
2019-10-23 16:12:18 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { useRoutes, navigate, A, usePath, useInterceptor } from 'hookrouter'
 
-function App() {
+const NavItem = ({ children, href, path }) => (
+  <li>
+    <A href={href} style={{ backgroundColor: path === href ? 'yellow' : 'transparent' }}>
+      {children}
+    </A>
+  </li>
+)
+
+const Layout = ({ children }) => {
+  const path = usePath()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <React.Fragment>
+      <header>
+        <nav>
+          <ul style={{ display: 'flex', gap: '1em', listStyleType: 'none', padding: 0 }}>
+            <NavItem href="/" path={path}>Form</NavItem>
+            <NavItem href="/redirect" path={path}>Redirect</NavItem>
+            <NavItem href="/other" path={path}>Other</NavItem>
+          </ul>
+        </nav>
       </header>
-    </div>
-  );
+      <hr />
+      {children}
+    </React.Fragment>
+  )
 }
 
-export default App;
+function intercept(currentPath, nextPath) {
+  if (confirm("Abandon changes?")) { // eslint-disable-line no-restricted-globals
+    return nextPath
+  }
+  return currentPath
+}
+
+const FormPage = () => {
+  useInterceptor(intercept)
+
+  return (
+    <main>
+      <h1>Form page</h1>
+      <p>Imagine a form with unsaved changes is here. When submitting the form
+        we stop the interceptor; this works fine.</p>
+      <p>If you click to navigate to the "other" page you'll get a confirmation
+        dialog, which works as expected whichever option is chosen.</p>
+      <p>If you click to navigate to the "redirect" page, which then redirects
+        you to the "other" page, you'll get the confirmation dialog once on the
+        first navigation intent, and then again when it redirects you to the
+        "other" page. This second dialog is what I want to avoid.</p>
+    </main>
+  )
+}
+
+const RedirectPage = () => {
+  navigate('/other')
+  return (
+    <main>
+      Navigating... (you'll see this if you click "yes" and then "no")
+    </main>
+  )
+}
+
+const OtherPage = () => <main><h1>Other page</h1></main>
+const NotFoundPage = () => <main><h1>Not found</h1></main>
+
+const routes = {
+  '/': () => <FormPage />,
+  '/redirect': () => <RedirectPage />,
+  '/other' : () => <OtherPage />,
+}
+
+function App() {
+  const routeResult = useRoutes(routes)
+  return <Layout>{routeResult || <NotFoundPage />}</Layout>
+}
+
+export default App
